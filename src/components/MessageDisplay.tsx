02bb2b23feb2e85c00f24d670eaeb42d4065b0db
TanLui <effishing@gmail.com>
2023-09-18 17:25:10 +0200

    styling ui

--- src/components/MessageDisplay.tsx
+++ src/components/MessageDisplay.tsx
@@ -1,4 +1,10 @@
 const MessageDisplay = () => {
-  return <div className='message-display'>Message Display</div>
+  return (
+    <div className='message-display'>
+      <p id='icon'>X</p>
+      <p>User</p>
+      <p>Message</p>
+    </div>
+  )
 }
 export default MessageDisplay
--- src/components/ResultDisplay.tsx
+++ src/components/ResultDisplay.tsx
@@ -1,4 +1,16 @@
 const ResultDisplay = () => {
-  return <div className='result-display'>Result Display</div>
+  return (
+    <div className='result-display'>
+      Result Display
+      <div className='buttons'>
+        <div className='button first'></div>
+        <div className='button middle'></div>
+        <div className='button last'></div>
+      </div>
+      <div className='result'>
+        <p> </p>
+      </div>
+    </div>
+  )
 }
 export default ResultDisplay
